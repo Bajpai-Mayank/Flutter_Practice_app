7368b08a2e7fb6729fac389d69200ee4833a1f32
Bajpai Mayank <bajpayimayank023@gmail.com>
2026-07-24 16:12:01 +0530

    refactor catalog model; implement data loading from JSON and update home page to display products

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,23 +2,52 @@ import 'package:flutter/material.dart';
 import 'package:my_first_app/models/catalog.dart';
 import 'package:my_first_app/widget/Items_widget.dart';
 import 'package:my_first_app/widget/drawer.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  Future<void> loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productData = decodedData["products"];
+    CatalogModel.products = List.from(
+      productData,
+    ).map((item) => Item.fromMap(item)).toList();
+    setState(() {
+      
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final dummyList =  List.generate(20,(index) => CatalogModel.products[0]);
+    // final dummyList = List.generate(20, (index) => CatalogModel.products[0]);
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Cataog App"),
-      ),
-      body:Padding(
+      appBar: AppBar(title: Text("Cataog App")),
+      body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: ListView.builder(
-          itemCount: dummyList.length,
+        child:(CatalogModel.products.isNotEmpty)? ListView.builder(
+          itemCount: CatalogModel.products.length,
           itemBuilder: (context, index) {
-             return ItemWidget(item: dummyList[index]);
+            return ItemWidget(item: CatalogModel.products[index]);
           },
+        ): Center(
+          child: CircularProgressIndicator(
+            backgroundColor:Colors.black,
+          ),
         ),
       ),
       drawer: MyDrawer(),
